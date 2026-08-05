e5fff21336a5c0a565e70bef71715154ef054e55
grandmaster-mak <jolupinlola@gmail.com>
2026-08-05 18:57:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2720,14 +2720,14 @@ function updateKingdomJourney(currentKingdom) {
 // Kingdom image path mapping
 function getKingdomImagePath(kingdomId) {
     const images = {
-        'village': 'pieces/kingdoms/village.png',
-        'town': 'pieces/kingdoms/town.png',
-        'fortress': 'pieces/kingdoms/fortress.png',
-        'city': 'pieces/kingdoms/city.png',
-        'kingdom': 'pieces/kingdoms/kingdom.png',
-        'grand-kingdom': 'pieces/kingdoms/grand_kingdom.png',
-        'dominion': 'pieces/kingdoms/dominion.png',
-        'empire': 'pieces/kingdoms/empire.png'
+        'village': 'pieces/village.png',
+        'town': 'pieces/town.png',
+        'fortress': 'pieces/fortress.png',
+        'city': 'pieces/city.png',
+        'kingdom': 'pieces/kingdom.png',
+        'grand-kingdom': 'pieces/grand_kingdom.png',
+        'dominion': 'pieces/dominion.png',
+        'empire': 'pieces/empire.png'
     };
     return images[kingdomId] || images['village'];
 }
